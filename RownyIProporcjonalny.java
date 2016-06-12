b81e51ef2de4abba3cf0a6534bb910a88452a120
Sebastian Szymanski <228060@student.pwr.edu.pl>
2016-06-12 23:46:37 +0200

    Równy i Proporcjonalny create

---
+++ RownyIProporcjonalny.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class RownyIProporcjonalny {
+
+	ArrayList<Proces> procesy = new ArrayList<>();
+	ArrayList<Ramka> ramki = new ArrayList<>();
+	ArrayList<Integer> przydzial = new ArrayList<>();
+	
+	public RownyIProporcjonalny(ArrayList<Proces> procesy, ArrayList<Ramka> ramki, ArrayList<Integer> przydzial)
+	{
+		this.procesy = procesy;
+		this.ramki = ramki;
+		this.przydzial = przydzial;
+	}
+	
+
+}
